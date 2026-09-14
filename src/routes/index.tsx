407d4a06b5542aac19df122f10fc1e3c91e9cdb2
sumanshetty17 <sumansheety17@gmail.com>
2026-09-14 13:55:06 +0530

    Delete src directory

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { HomePage } from "@/components/site/home-page";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <HomePage />;
-}
