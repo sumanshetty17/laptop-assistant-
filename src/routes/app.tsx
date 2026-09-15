ff7ce7d73c9667c78e86cdfb870f438f00117a02
sumanshetty17 <sumansheety17@gmail.com>
2026-09-15 21:46:35 +0530

    Delete src directory

--- src/routes/app.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { OrbitApp } from "@/components/orbit-app";
-
-export const Route = createFileRoute("/app")({ component: AppPage });
-
-function AppPage() {
-  return <OrbitApp />;
-}
